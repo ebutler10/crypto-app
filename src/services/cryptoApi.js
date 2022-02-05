c02a6e76873ec35ccc7a549a5d35b9308975f6fe
Emily Butler <emilybutler382@gmail.com>
2022-02-04 18:25:33 -0600

    messing with api

--- src/services/cryptoApi.js
+++ src/services/cryptoApi.js
@@ -1,15 +1,24 @@
-//var options = {
-  //method: 'GET',
-  //url: 'https://coinranking1.p.rapidapi.com/exchanges',
-  //params: {
-    //referenceCurrencyUuid: 'yhjMzLPhuIDl',
-    //limit: '50',
-    //offset: '0',
-    //orderBy: '24hVolume',
-    //orderDirection: 'desc'
-  //},
-  //headers: {
-    //'x-rapidapi-host': 'coinranking1.p.rapidapi.com',
-    //'x-rapidapi-key': 'f202df29cfmshd2f75362c20ca73p18c695jsn8338ca6f4cc4'
-  //}
-//};
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+
+const cryptoApiHeaders = {
+  'x-rapidapi-host': "https://api.binance.com/api/v3/exchangeInfo",
+  //'x-rapidapi-key': 'f202df29cfmshd2f75362c20ca73p18c695jsn8338ca6f4cc4'
+};
+
+const baseUrl = "https://api.binance.com/api/v3/exchangeInfo"
+
+const createRequest = (url) => ({ url, headers: cryptoApiHeaders });
+
+export const cryptoApi = createApi({
+  reducerPath: "cryptoApi",
+  baseQuery: fetchBaseQuery({ baseUrl }),
+  endpoints: (builder) => ({
+    getCryptos: builder.query({
+      query: () => createRequest("/coins"),
+    }),
+  }),
+});
+
+export const {
+  useGetCryptosQuery,
+} = cryptoApi;
